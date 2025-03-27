62ba53c3ad140773d9d98769df9aa7d83fe155fb
ssannejohansson <sannej0hansson@hotmail.com>
2025-03-27 09:24:07 +0100

    Exercise 2 done
    exercise 2 done

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
 // Assignment 4
 
-/* EXERCISE 1: Guess the random number 
+// Exercise 1
 
-Create a function guessNumberGame that generates a random number between 1 and 10. 
-
-The function should repeatedly prompt the user to guess the number until the correct number is guessed. 
-
-Use a while loop to keep asking the user for input until they guess correctly.  
-
-Provide feedback if the guess is too high or too low. */
-
-const target = Math.floor(Math.random() * 10);
+/* const target = Math.floor(Math.random() * 9 + 1);
 let guess = null;
 
 while (guess !== target) {
     console.log(`Target: ${target} Guess: ${guess}`);
     guess = parseFloat(prompt("Guess the number!")); 
-}
 
-console.log(`Target: ${target} Guess: ${guess}`);
-console.log("You guessed the right number!");
+    if (guess < target) {
+            alert("Higher...");
+        } else if (guess > target) {
+            alert("Lower..."); 
+        } else {
+            console.log(`Target: ${target} Guess: ${guess}`);
+            console.log("You guessed the right number!"); 
+        }
+    }
+*/
 
+// Exercise 2
 
-/* EXERCISE 2: Create an array of random numbers 
+function generateRandomArray() {
+    for (i = 0; i <1; i++) {
+    console.log(Array.from ({
+        length: 7
+    }, () =>  Math.floor(Math.random() * 100) +1))
+    }
+   }
 
-Create a function generateRandomArray that accepts one argument: length (the length of the array). 
-
-Inside the function, use a for loop to generate an array of random numbers between 1 and 100. 
-
-Return the array from the function. */
-
- 
-
- 
-
-/* EXERCISE 3: Calculate the sum of even numbers in the array 
-
-Create a function sumEvenNumbers that accepts an array as an argument. 
-
-Use a for loop to iterate through the array and calculate the sum of the even numbers. 
-
-Return the sum of the even numbers. */
+generateRandomArray(); 
